25fb19d19e56cb925c04d5677b01beb2d29af73f
秦璞 <116573916@qq.com>
2018-03-15 18:45:27 +0800

    Update env.js

--- config/env.js
+++ config/env.js
@@ -6,19 +6,7 @@
  */
 export default {
   production: {
-    APIURL: 'https://zlcx.sunlands.com/',
-    APIV2: 'https://mp.sunlands.wang/',
-    IMURL: 'https://msg.sunlands.wang/mp-message-service/',
-    ASSETSURL: 'https://img.sunlands.wang/',
-    DEVGIF: 'https://mp.sunlands.wang/',
-    APPID: 'wx77fcb7f2e576acd5',
   },
   dev: {
-    APIURL: 'https://www.sunlands.wang/',
-    APIV2: 'https://devmsg.sunlands.wang/',
-    ASSETSURL: 'https://devimg.sunlands.wang/',
-    IMURL: 'https://devmsg.sunlands.wang/mp-message-service/',
-    DEVGIF: 'https://devgif.sunlands.wang/',
-    APPID: 'wx485dd1b0372e6a36',
   }
 }
